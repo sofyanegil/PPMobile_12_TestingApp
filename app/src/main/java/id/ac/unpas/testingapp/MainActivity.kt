c73f3ef273bec3486f7bef171e396d8c90a20bd8
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-05-27 08:40:37 +0700

    refactor MainActivity

--- app/src/main/java/id/ac/unpas/testingapp/MainActivity.kt
+++ app/src/main/java/id/ac/unpas/testingapp/MainActivity.kt
@@ -3,13 +3,21 @@ package id.ac.unpas.testingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import id.ac.unpas.testingapp.ui.theme.TestingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +26,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestingAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var name = remember { mutableStateOf("") }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column {
+            Greeting(name.value)
+            Button(
+                onClick = { name.value = "Android" },
+                modifier = Modifier
+                    .testTag("buttonGo")
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "GO")
             }
         }
     }
